3c8482efdb3fd4abd350191c7ad13716dc7ff29a
onfrostyfire-hub <onfrostyfire@gmail.com>
2026-04-24 05:49:39 -0300

    Update fish_mobile.py

--- views/fish_mobile.py
+++ views/fish_mobile.py
@@ -662,7 +662,7 @@ def show():
     </div>"""
 
     html = f'<div class="mobile-game-area {combo_cls} {table_status_class}">{shatter_html}<div class="pf-pot-badge"><div class="pot-badge-mob">Pot: {pot_size} bb</div></div><div class="pf-board"><div class="board-container-mob">{board_html}</div></div>{mastery_html}{opp_html}{chips_html}<div class="hero-mob">{anim_html}<div class="hero-cards-wrap"><div class="card-mob"><div class="tl-mob {c1}">{r1}<br>{s1}</div><div class="c-mob {c1}">{s1}</div></div><div class="card-mob"><div class="tl-mob {c2}">{r2}<br>{s2}</div><div class="c-mob {c2}">{s2}</div></div><div class="rng-badge">{st.session_state.pf_rng}</div></div><div class="hero-plate"><span class="pos">HERO {hero_pos}</span><span class="stack">{hero_stack}</span></div></div></div>'
-    
+
     st.markdown(html, unsafe_allow_html=True)
 
     def handle_action(action):
